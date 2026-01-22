f0062c86189d877e7c3d46043fbda365ca21c7e0
DanielOlivo <vit.masterov@gmail.com>
2026-01-22 19:56:31 +0200

    BasicNote logic update

--- src/notes/components/BasicNote/BasicNote.tsx
+++ src/notes/components/BasicNote/BasicNote.tsx
@@ -1,35 +1,56 @@
-import type { FC } from "react"
-import type { BasicNoteDto } from "../../notes.dto"
-import { useGetNoteConfigQuery } from "../../../grid/grid.api"
-import { useOpeningHook } from "../../../grid/hooks"
+import { useEffect, useState, type FC } from "react"
+import type { IBasicNote as BasicNoteRecord } from "../../../db/entities/Note"
+import { useGetIntervalQuery } from "../../note.api"
+import dayjs from "dayjs"
+import duration from "dayjs/plugin/duration"
 
-export interface BasicNoteProps {
-    note: BasicNoteDto
-    gridId: string
-}
+dayjs.extend(duration)
 
-export const BasicNote: FC<BasicNoteProps> = ({note: {id, front, back}, gridId}) => {
+export const BasicNote: FC<BasicNoteRecord> = ({id, front, back}) => {
 
-    const { data: config, isError, isLoading, isUninitialized } = useGetNoteConfigQuery({noteId: id, gridId});
-    const { isOpen } = useOpeningHook(config?.lastOpenTimestamp, config?.currentOpenInterval, isUninitialized || isLoading);
+    const { data: interval, isError, isLoading } = useGetIntervalQuery(id)
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null)
+    const [isOpen, setIsOpen] = useState<boolean | null>(null)
+    const [remained, setReamined] = useState<string | null>(null)
 
-    if(isLoading || isUninitialized){
-        return <div>Loading...</div>
-    }
+    useEffect(() => {
+        if(!isError && !isLoading && interval){
+            const closeTime = interval.openTimestamp + interval.openDuration
+            const opened = Date.now() < closeTime
+            setIsOpen(opened)
 
-    if(isError){
-        return <div>Error occurred</div>
-    }
+            const reaminedUpdater = () => {
+                const rem = Math.floor((closeTime - Date.now()))
+                if(rem < 0){
+                    if(intervalId) clearInterval(intervalId)
+                    setIntervalId(null)
+                    setIsOpen(false)
+                    setReamined(null)
+                }
+                else {
+                    const formatted = dayjs.duration(rem / 1000, 'seconds').format("HH:mm:ss")
+                    setReamined(formatted)
+                }
+            }
 
-    if(!config){
-        return <div>config is null</div>
-    }
+            if(opened){
+                const int = setInterval(reaminedUpdater, 1000)
+                setIntervalId(int)
+            }
+
+            return () => {
+                if(intervalId){
+                    clearInterval(intervalId)
+                    setIntervalId(null)
+                }
+            }
+        }
+    }, [interval, isError, isLoading])
 
     return (
-        <div className="w-full h-full">
-            <span style={{
-                color: isOpen ? "green" : "red"
-            }}>{isOpen ? front : back}</span>
+        <div className="w-full h-full flex justify-center items-center">
+            <span>{front} - {isOpen ? back : "_______"}</span>
+            {remained && <span>{remained}</span>}
         </div>
     )
 }
