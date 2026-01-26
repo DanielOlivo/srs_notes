74ba30d5435470235740515e6df56560b558c77e
DanielOlivo <vit.masterov@gmail.com>
2026-01-26 18:50:38 +0200

    StoreState

---
+++ src/utils/StoreState.ts
@@ -0,0 +1,77 @@
+import { v4 } from "uuid";
+import type { RootState, StoreState } from "../app/store";
+import type { Document } from "../db/entities/document";
+import type { GridApiData } from "../grid/grid.api";
+import type { NoteApiData } from "../notes/note.api";
+import type { Note, NoteData } from "../db/entities/Note";
+import type { Position } from "../db/entities/position";
+import type { Interval } from "../db/entities/interval";
+import { faker } from "@faker-js/faker";
+
+type SlicesOnlyState = Omit<RootState, "api">
+
+export class StoreStateUtility implements StoreState {
+    slices?: Partial<SlicesOnlyState>  = undefined
+    gridApi?: GridApiData = undefined
+    noteApi: NoteApiData = {
+        documents: [],
+        notes: [],
+        positions: [],
+        intervals: [],
+    }
+
+    addDocument(name: string, type: Document["type"]) {
+        const document: Document = {
+            id: v4(),
+            name,
+            type,
+            createdAt: Date.now(),
+        }
+        this.noteApi.documents.push(document)
+        return document
+    } 
+
+    static getNote(data: NoteData): Note {
+        return {
+            ...data,
+            id: v4(),
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+    }
+
+    addNote(document: Document, data: NoteData) {
+        if(document.type === 'grid'){
+            throw new Error("not implemented")
+        }
+
+        const note = StoreStateUtility.getNote(data)
+        const positions = this.noteApi.positions.filter(pos => pos.documentId === document.id)
+        const maxY = Math.max(...positions.map(pos => pos.coord.y), 0)
+        const position: Position = {
+            id: this.noteApi.positions.length + 1,
+            noteId: note.id,
+            documentId: document.id,
+            coord: {
+                x: 0,
+                y: maxY + 1
+            }
+        }
+        this.noteApi.notes.push(note)
+        this.noteApi.positions.push(position)
+        return { note, position }
+    }
+
+    addInterval(note: Note, data?: Omit<Interval, 'id' | 'noteId'>){
+        const id = v4()
+        const interval: Interval = {
+            id,
+            noteId: note.id,
+            openTimestamp: data?.openTimestamp ?? faker.date.recent().getTime(),
+            openDuration: data?.openDuration ?? Math.random() * 20000,
+            ...data
+        }
+        this.noteApi.intervals.push(interval)
+        return interval
+    }
+}
