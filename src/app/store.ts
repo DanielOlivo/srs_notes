e127573ce2eb625b3c9d99f505df99f54b72efbf
DanielOlivo <vit.masterov@gmail.com>
2026-01-26 19:12:47 +0200

    store - references to grid were removed

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import gridReducer, { initialState as gridInitialState } from '../grid/grid.slice'
+// import gridReducer, { initialState as gridInitialState } from '../grid/grid.slice'
 import { api } from "../api";
-import { GridApiUtils, type GridApiData } from "../grid/grid.api";
+// import { GridApiUtils, type GridApiData } from "../grid/grid.api";
 import { handleNoteCache, type NoteApiData } from "../notes/note.api";
 
 
 export const getStore = () => configureStore({
     reducer: {
-        gridReducer,
+        // gridReducer,
         [api.reducerPath]: api.reducer
     },
     middleware: (getDefaultMiddleware) =>
@@ -25,20 +25,20 @@ type SlicesOnlyState = Omit<RootState, "api">
 
 export type StoreState = {
     slices?: Partial<SlicesOnlyState>
-    gridApi?: GridApiData
+    // gridApi?: GridApiData
     noteApi?: NoteApiData
 }
 
 
-export const getStoreWithState = ({slices, gridApi, noteApi}: StoreState) => {
+export const getStoreWithState = ({noteApi}: StoreState) => {
 
     const initialState: SlicesOnlyState = {
-        gridReducer: slices?.gridReducer ?? gridInitialState,
+        // gridReducer: slices?.gridReducer ?? gridInitialState,
     }
 
     const store = configureStore({
         reducer: {
-            gridReducer,
+            // gridReducer,
             [api.reducerPath]: api.reducer
         },
         middleware: (getDefaultMiddleware) =>
@@ -46,10 +46,10 @@ export const getStoreWithState = ({slices, gridApi, noteApi}: StoreState) => {
         preloadedState: initialState
     })
 
-    if(gridApi){
-        const utils = new GridApiUtils(store)
-        utils.apply(gridApi)
-    }
+    // if(gridApi){
+    //     const utils = new GridApiUtils(store)
+    //     utils.apply(gridApi)
+    // }
 
     if(noteApi){
         handleNoteCache(noteApi, store)
