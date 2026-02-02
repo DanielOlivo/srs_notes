228ef801985f55394aa5610fca10c14b6f8ee6f4
DanielOlivo <vit.masterov@gmail.com>
2026-02-02 08:06:18 +0200

    Answer class

--- src/db/entities/answer.ts
+++ src/db/entities/answer.ts
@@ -1,4 +1,6 @@
-import type { Ease } from "../../Game/Ease"
+import { parse } from 'papaparse'
+
+export type Ease = 'Again' | 'Hard' | 'Good' | 'Easy'
 
 export interface IAnswer {
     id: string
@@ -16,5 +18,43 @@ export interface AnswerDb {
         indexes: {
             "by-noteId": string,
         }
+    }
+}
+
+export class Answer implements IAnswer {
+    id: string
+    noteId: string
+    answer: Ease
+    timestamp: number
+
+    constructor(id: string, noteId: string, answer: Ease, timestamp: number){
+        this.id = id
+        this.noteId = noteId
+        this.answer = answer
+        this.timestamp = timestamp
+    }
+
+    asPlain = (): IAnswer => ({
+        id: this.id,
+        noteId: this.noteId,
+        answer: this.answer,
+        timestamp: this.timestamp
+    })
+
+    toCsvRow = ():string => `${this.id},${this.noteId},${this.answer},${this.timestamp}`
+
+    static fromCsv(csv: string): Answer[] {
+        const data = parse(csv)
+        const records = data.data as string[][]
+        const answers = records.map(row => {
+            const [ id, noteId, answerStr, timestampStr ] = row
+            return new Answer(
+                id,
+                noteId,
+                answerStr as Ease,
+                parseInt(timestampStr)
+            )
+        })
+        return answers
     }
 }
