7e995f82e3afafbc08e291400ad172af4b4da770
DanielOlivo <vit.masterov@gmail.com>
2026-02-02 07:59:16 +0200

    Position class

--- src/db/entities/position.ts
+++ src/db/entities/position.ts
@@ -1,4 +1,5 @@
 import type { IVector2 } from "../../utils/Vector2"
+import { parse } from 'papaparse'
 
 export interface IPosition {
     id: number
@@ -20,4 +21,43 @@ export interface PositionDb {
             "by-docId": string
         }
     }
+}
+
+export class Position implements IPosition {
+    id: number
+    noteId: string
+    documentId: string
+    coord: IVector2
+
+    constructor(id: number, noteId: string, documentId: string, coord: IVector2){
+        this.id = id
+        this.noteId = noteId
+        this.documentId = documentId
+        this.coord = coord
+    }   
+
+    asPlain = (): IPosition => ({
+        id: this.id,
+        noteId: this.noteId,
+        documentId: this.documentId,
+        coord: this.coord
+    })
+
+    toCsvRow = () => `${this.id},${this.noteId},${this.documentId},${this.coord.x},${this.coord.y}`
+
+    static fromCsv(csv: string){
+        const data = parse(csv)
+        const records = data.data as string[][]
+        const positions = records.map(row => {
+            const [ id, noteId, documentId, xStr, yStr ] = row
+            return new Position(
+                parseInt(id),
+                noteId,
+                documentId,
+                { x: parseInt(xStr), y: parseInt(yStr) }
+            )
+        })
+        return positions
+    }
+
 }
