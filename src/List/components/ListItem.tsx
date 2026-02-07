643fa30e44a25841079b16507ae24d2659bbb1bb
DanielOlivo <vit.masterov@gmail.com>
2026-02-07 13:32:24 +0200

    note edit button appears only on edit mode

--- src/List/components/ListItem.tsx
+++ src/List/components/ListItem.tsx
@@ -2,6 +2,8 @@ import type { FC } from "react";
 import { Note } from "../../notes/components/Note/Note";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router";
+import { useAppSelector } from "../../app/hooks";
+import { selectMode } from "../list.selectors";
 
 export interface ListItemProps {
     id: string
@@ -13,6 +15,11 @@ export interface ListItemProps {
  * @returns 
  */
 export const ListItem: FC<ListItemProps> = ({id}) => {
+
+    const mode = useAppSelector(selectMode)
+
+    const showEditButton = mode.kind === 'edit' || mode.kind === 'onUpdate'
+
     return (
         <li 
             className="w-full h-20 
@@ -26,24 +33,27 @@ export const ListItem: FC<ListItemProps> = ({id}) => {
             </div>
 
             <div className="flex-none w-6">
-                <button
-                    popoverTarget={`popover-${id}`} 
-                    style={{ anchorName: `--anchor-${id}`} as React.CSSProperties}
-                >
-                    <EllipsisVerticalIcon className="size-5" />
-                </button>
+                {showEditButton &&
+                    <>
+                        <button
+                            popoverTarget={`popover-${id}`} 
+                            style={{ anchorName: `--anchor-${id}`} as React.CSSProperties}
+                        >
+                            <EllipsisVerticalIcon className="size-5" />
+                        </button>
 
-                <ul 
-                    className="dropdown dropdown-end menu w-24 rounded-box bg-neutral"
-                    popover="auto"
-                    id={`popover-${id}`}
-                    style={{ positionAnchor: `--anchor-${id}`} as React.CSSProperties}
-                >
-                    <li><a className="hover:bg-accent">add above</a></li>
-                    <li><Link to={`noteEdit/${id}`}>edit</Link></li>
-                    <li><a>add below</a></li>
-                </ul>
-
+                        <ul 
+                            className="dropdown dropdown-end menu w-24 rounded-box bg-neutral"
+                            popover="auto"
+                            id={`popover-${id}`}
+                            style={{ positionAnchor: `--anchor-${id}`} as React.CSSProperties}
+                        >
+                            <li><a className="hover:bg-accent">add above</a></li>
+                            <li><Link to={`noteEdit/${id}`}>edit</Link></li>
+                            <li><a>add below</a></li>
+                        </ul>
+                    </>
+                }
             </div>
         </li>
     )
