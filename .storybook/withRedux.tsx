7735641c9d8bafb0e9d0ea7cf85b053b325677b2
DanielOlivo <vit.masterov@gmail.com>
2026-02-22 11:13:21 +0200

    withRedux decorator updates time

--- .storybook/withRedux.tsx
+++ .storybook/withRedux.tsx
@@ -1,12 +1,16 @@
 import { type Decorator } from "@storybook/react-vite";
 import { getStore, getStoreWithState, type StoreState } from "../src/app/store";
 import { Provider } from "react-redux";
+import { useIncrementTime } from "../src/List/hooks/incrTime"
+import { setTime } from "../src/List/list.slice";
 
 export const withRedux: Decorator = (Story, context) => {
       const { redux, /* styles */ } = context.parameters 
 
       const store = redux ? getStoreWithState(redux as StoreState) : getStore()
       // const _styles = styles as React.CSSProperties ?? {}
+
+      setInterval(() => store.dispatch(setTime()), 1000)
 
       return (
         <Provider store={store}>
